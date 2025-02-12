3306c939ce45cae888c343de22277ef5d165b212
Vigus Widdicombe Gasson <vwiddicombegasson@gmail.com>
2025-02-12 22:12:18 +0000

    Added stockdata endpoint

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from flask import Flask, json
+import yfinance
+import pandas
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
-    return 'Hello Ace'
+    return 'random github'
+
+@app.route('/api/stocks')
+def getstocks():
+    return {}
+
+@app.route('/api/stockdata')
+def stockd():
+    thingy = yfinance.Ticker("MSFT")
+    a: pandas.DataFrame = thingy.history(period="5y")
+    print(a)
+    x = {"labels": a.index.strftime('%d-%m-%Y').tolist(), "Opens": a.get('Open').tolist(), "Closes": a.get('Close').tolist()}
+    return x
+
+app.run()
