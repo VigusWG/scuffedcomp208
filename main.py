d5c3aceceb92c96aff7dc2eb23e90ef96b08b3ec
Vigus Widdicombe Gasson <vwiddicombegasson@gmail.com>
2025-02-12 22:36:37 +0000

    All backend

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, json
+from flask import Flask, json, request
 import yfinance
-import pandas
+import csv
 
 app = Flask(__name__)
+stocks = {}
+with open('stock_info.csv') as r:
+    reader = csv.reader(r)
+    for row in reader:
+        stocks[row[1]] = row[0]
 
 @app.route('/')
 def main():
@@ -10,13 +15,21 @@ def main():
 
 @app.route('/api/stocks')
 def getstocks():
-    return {}
+    search = request.args.get('s')
+    if not search:
+        return {"error": "enter something"}
+    return {"data": [[i, stocks[i]] for i in stocks.keys() if i.lower().startswith(search.lower())][:10]}
 
 @app.route('/api/stockdata')
 def stockd():
-    thingy = yfinance.Ticker("MSFT")
-    a: pandas.DataFrame = thingy.history(period="5y")
-    print(a)
+    stock = request.args.get('s')
+    if not stock:
+        return {"error": "enter something"}
+    thingy = yfinance.Ticker(stock)
+    # if not thingy.fast_info():   ## fuck it theyre big boys we dont need to error handle this for now
+    #     return {"error": "Unknown stock"}
+    
+    a = thingy.history(period="5y")
     x = {"labels": a.index.strftime('%d-%m-%Y').tolist(), "Opens": a.get('Open').tolist(), "Closes": a.get('Close').tolist()}
     return x
 
